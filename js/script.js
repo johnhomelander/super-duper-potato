14fa2d91652aa7ceda1b6902950d58ed8175413c
johnhomelander <sahnipranav10@gmail.com>
2025-01-08 10:07:04 +0530

    Fixing errors

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 const display = document.getElementById('display');
 
-        function appendValue(value) {
-            display.value += value;
-        }
+function appendValue(value) {
+    display.value += value;
+}
 
-        function clearDisplay() {
-            display.value = '';
-        }
+function clearDisplay() {
+    display.value = '';
+}
 
-        function calculate() {
-            try {
-                display.value = eval(display.value);
-            } catch (error) {
-                alert('Invalid calculation');
-                clearDisplay();
-            }
-        }
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        alert('Invalid calculation');
+        clearDisplay();
+    }
+}
